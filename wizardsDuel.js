f85ddd1163b55239f8c1956177d905a67957e22c
Dylan Hutchison <hutchisond@dewv.edu>
2018-10-17 13:29:32 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -10,13 +10,20 @@ class PlayerWizard extends Sprite {
         this.x = this.width;
         this.y = this.y;
         this.defineAnimation("down", 6, 8);
-        this.defineAnimation('right', 3,5);
+        this.defineAnimation('right', 3, 5);
+        this.defineAnimation('up', 0, 2);
         this.speedWhenWalking = 100;
+        this.spellCastTime = 0;
     }
     handleDownArrowKey() {
         this.playAnimation("down");
         this.speed = this.speedWhenWalking;
         this.angle = 270;
+    }
+    handleUpArrowKey() {
+        this.playAnimation('up');
+        this.speed = this.speedWhenWalking;
+        this.angle = 90;
     }
     handleGameLoop() {
         this.y = Math.max(5, this.y);
@@ -26,16 +33,92 @@ class PlayerWizard extends Sprite {
         this.speed = 0;
     }
     handleSpacebar() {
-        let spell = new Spell();
-        spell.x = this.width + this.x;
-        spell.y = this.y;
-        spell.name = "A spell cast by Marcus";
-        spell.angle = 0;
-        spell.setImage('marcusSpellSheet.png');
-        this.playAnimation('right');
-
+        let now = game.getTime();
+        if (now - this.spellCastTime >= 2) {
+            this.spellCastTime = now;
+            let spell = new Spell();
+            spell.x = this.width + this.x;
+            spell.y = this.y;
+            spell.name = "A spell cast by Marcus";
+            spell.angle = 0;
+            spell.setImage('marcusSpellSheet.png');
+            this.playAnimation('right');
+        }
     }
 }
+
+class NonPlayerWizard extends Sprite {
+    constructor() {
+        super();
+        this.name = "The mysterious stranger";
+        this.setImage("strangerSheet.png");
+        this.width = 48;
+        this.height = 48;
+        this.x = game.displayWidth - 2 * this.width;
+        this.y = this.height;
+        this.angle = 270;
+        this.speed = 150;
+        this.defineAnimation("up", 0, 2);
+        this.defineAnimation("down", 6, 8);
+        this.defineAnimation("left", 9, 11);
+        this.playAnimation('down');
+    }
+    handleGameLoop() {
+        if (this.y <= 0) {
+            // Upward motion has reached top, so turn down
+            this.y = 0;
+            this.angle = 270;
+            this.playAnimation("down");
+        }
+        if (this.y >= game.displayHeight - this.height) {
+            // Downward motion has reached bottom, so turn up
+            this.y = game.displayHeight - this.height;
+            this.angle = 90;
+            this.playAnimation("up");
+        }
+        if (Math.random() < 0.01) {
+            let sspell = new Spell();
+            sspell.x = this.x - this.width;
+            sspell.y = this.y;
+            sspell.name = "A spell cast by stranger";
+            sspell.angle = 180;
+            sspell.setImage('strangerSpellSheet.png');
+            this.playAnimation('left');
+        }
+    }
+    handleAnimationEnd() {
+        if (this.angle === 90) {
+            this.playAnimation("up");
+        }
+        if (this.angle === 270) {
+            this.playAnimation("down");
+        }
+    }
+}
+
+class Fireball extends Sprite {
+    constructor(deadSprite) {
+        super();
+        this.x = deadSprite.x;
+        this.y = deadSprite.y;
+        this.setImage('fireballSheet.png');
+        game.removeSprite(deadSprite);
+        this.defineAnimation('explode', 0, 16);
+        this.playAnimation('explode');
+    }
+    handleAnimationEnd() {
+        game.removeSprite(this);
+        if (!game.isActiveSprite(stranger)) {
+            game.end("Congratulations!\n\nMarcus has defeated the mysterious" +
+                "\nstranger in the dark cloak!");
+        }
+        if (!game.isActiveSprite(marcus)) {
+            game.end("Marcus is defeated by the mysterious\nstranger in the dark cloak!\n\nBetter luck next time.");
+        }
+    }
+}
+
+let stranger = new NonPlayerWizard();
 
 class Spell extends Sprite {
     constructor() {
@@ -49,6 +132,16 @@ class Spell extends Sprite {
     }
     handleBoundaryContact() {
         game.removeSprite(this);
+    }
+    handleCollision(otherSprite) {
+        if (this.getImage() !== otherSprite.getImage()) {
+            let verticalOffset = Math.abs(this.y - otherSprite.y);
+            if (verticalOffset < this.height / 2) {
+                game.removeSprite(this);
+                new Fireball(otherSprite);
+            }
+        }
+        return false;
     }
 }
 let marcus = new PlayerWizard();
